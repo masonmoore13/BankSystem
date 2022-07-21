c8cbc21285299425a03f0a5cf55fcb985c16f223
Mason Moore <97636966+masonmoore13@users.noreply.github.com>
2022-07-21 15:17:33 -0500

    Update CustomerService.java

--- src/main/java/com/lq/bank/service/CustomerService.java
+++ src/main/java/com/lq/bank/service/CustomerService.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.stereotype.Service;
+import com.lq.bank.model.Customer;
 
 @Service
 public class CustomerService {
